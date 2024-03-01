62955ea8325be212151c6aa91205609e82ef6cde
MUTHIAH_MURALIDHARAN <muralipkmsaravanan@gmail.com>
2024-03-01 19:55:57 +0530

    First commit for EmailService

---
+++ src/test/java/com/example/backendprojectmodule_emailservice/BackendProjectModuleEmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendprojectmodule_emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProjectModuleEmailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
